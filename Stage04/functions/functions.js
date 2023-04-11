600c677e5a1d1da450120a6d1113849e8a55c6df
Marcio Tanan <marcio-tanan@hotmail.com>
2023-04-11 15:56:43 -0300

    Update functions.js

--- Stage04/functions/functions.js
+++ Stage04/functions/functions.js
@@ -8,4 +8,4 @@ let number2 = 25
 
 console.log(`O número 1 é ${number1}`)
 console.log(`O número 2 é ${number2}`)
-console.log(`A soma é  ${sum(number1, number2)}`)
+console.log(`A soma é  ${sum(number1, number2)}`)//
