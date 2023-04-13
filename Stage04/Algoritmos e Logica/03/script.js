4d0a87b8c87b8667c353eb8158ee029c7f56769a
Marcio Tanan <marcio-tanan@hotmail.com>
2023-04-13 11:21:33 -0300

    Update script.js

--- Stage04/Algoritmos e Logica/03/script.js
+++ Stage04/Algoritmos e Logica/03/script.js
@@ -1,8 +1,17 @@
 alert("Iremos fazer operações com 2 números")
 let numberOne = prompt("Digite o primeiro número: ")
 let numberTwo = prompt("Digite o segundo número: ")
-alert("A soma dos Números é: " + (Number(numberOne) + Number(numberTwo)))
-alert("A subtração dos Números é: " + (Number(numberOne) - Number(numberTwo)))
-alert("A multiplicação dos Números é: " + (Number(numberOne) * Number(numberTwo)))
-alert("A divisão dos Números é: " + (Number(numberOne) / Number(numberTwo)))
-alert("O resto dos Números é: " + (Number(numberOne) % Number(numberTwo)))
+
+numberOne = Number(numberOne)
+numberTwo = Number(numberTwo)
+
+
+alert("A soma dos Números é: " + (numberOne + numberTwo))
+
+alert("A subtração dos Números é: " + (numberOne - numberTwo))
+
+alert("A multiplicação dos Números é: " + (numberOne * numberTwo))
+
+alert("A divisão dos Números é: " + (numberOne / numberTwo))
+
+alert("O resto dos Números é: " + (numberOne % numberTwo))
