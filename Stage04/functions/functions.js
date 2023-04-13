e99ea19043a7469c154395b049b6d04a6d6fc5d1
Marcio Tanan <marcio-tanan@hotmail.com>
2023-04-13 08:46:20 -0300

    Update functions.js

--- Stage04/functions/functions.js
+++ Stage04/functions/functions.js
@@ -1,3 +1,5 @@
+/*
+
 let temperature = 37
 
 if(temperature >= 37.5){
@@ -7,3 +9,51 @@ if(temperature >= 37.5){
 }else {
   console.log('Saudável')
 }
+
+
+let expression = 'i'
+
+switch (expression ){
+  case 'a':
+    console.log('a')
+    break
+  case 'b':
+    console.log('b')
+    break
+  default:
+    console.log('default') 
+    break   
+}
+*/
+
+function calculate(number1, operator, number2) {
+  let result
+
+  switch (operator) {
+    case '+':
+      result = number1 + number2
+      break
+    case '-':
+      result = number1 - number2
+      break
+    case '*':
+      result = number1 * number2
+      break
+    case '/':
+      result = number1 / number2
+      break
+    default:
+      console.log('não implementado')
+      break
+  }
+
+  return result
+
+
+
+
+
+
+}
+
+console.log(calculate(4, '+', 8))
