86039b388ebebfcbce06dd85c008bd9391d6931d
Marcio Tanan <marcio-tanan@hotmail.com>
2023-04-15 09:03:26 -0300

    updates

--- Stage04/Algoritmos e Logica/06/script.js
+++ Stage04/Algoritmos e Logica/06/script.js
@@ -1,7 +1,7 @@
 let result = prompt ("Advinhe o número que estou pensando? Está entre 0 e 10")
 const randomNumber = Math.round(Math.random() * 10 )
 
-let xAttempts = 1;
+
 
 
 while(Number(result) != randomNumber){
