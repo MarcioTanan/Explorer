00c00bc70f4c77da14f957e05d0e683286456518
Marcio Tanan <marcio-tanan@hotmail.com>
2023-04-15 15:12:07 -0300

    create new project

--- Stage05/Jogo adivinhação/script.js
+++ Stage05/Jogo adivinhação/script.js
@@ -1,32 +1,46 @@
+//Variaveis
 const screen1=document.querySelector(".screen1")
 const screen2=document.querySelector(".screen2")
-const randomNumber = Math.round(Math.random() * 10 )
-
+let randomNumber = Math.round(Math.random() * 10 )
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
 let xAttempts = 1;
 
+//Eventos
+btnTry.addEventListener("click", handleTryClick)
+btnReset.addEventListener("click", handleResetClick)
+document.addEventListener("keydown", function(e){
+  if(e.key == 'Enter' && screen1.classList.contains("hide")){
+  handleResetClick()
+  }
+})
+
+//Funções callback
 function handleTryClick(event){
   event.preventDefault()
 
   const inputNumber = document.querySelector("#inputNumber")
   
   if(Number(inputNumber.value) == randomNumber){
-    screen1.classList.add("hide")
-    screen2.classList.remove("hide")
-
-    document.querySelector(".screen2 h2").innerText = `Acertou em ${xAttempts} tentativas`
-    
-  }
+    toggleScreen()
+    screen2.querySelector("h2").innerText = `Acertou em ${xAttempts} tentativas`
+ }
  inputNumber.value = ""
  xAttempts++
 }
 
-
-const btnTry = document.querySelector("#btnTry")
-const btnReset = document.querySelector("#btnReset")
-
-btnTry.addEventListener("click", handleTryClick)
-btnReset.addEventListener("click", function(){
-  screen1.classList.remove("hide")
-  screen2.classList.add("hide")
+function handleResetClick(){
+  toggleScreen()
   xAttempts = 1
-})
+  randomNumber = Math.round(Math.random() * 10 )
+}
+
+function toggleScreen(){
+  screen1.classList.toggle("hide")
+  screen2.classList.toggle("hide")
+}
+
+
+
+
+
