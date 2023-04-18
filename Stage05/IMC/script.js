065fc803f81449e7eaba32e207e0d0c4ebcf782e
Marcio Tanan <marcio-tanan@hotmail.com>
2023-04-18 10:20:48 -0300

    update

--- Stage05/IMC/script.js
+++ Stage05/IMC/script.js
@@ -1,30 +1,42 @@
-// Variaveis
-
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-const modalWrapper = document.querySelector('.modal-wrapper')
-const modalMessage = document.querySelector('.modal .title span')
-const modalBtnClose = document.querySelector('.modal button.close')
+const Modal = {
+
+  wrapper: document.querySelector('.modal-wrapper'),
+  message: document.querySelector('.modal .title span'),
+  btnClose: document.querySelector('.modal button.close'),
+
+
+  open: function() {
+    Modal.wrapper.classList.add('open')
+  },
+  close: function() {
+    Modal.wrapper.classList.remove('open')
+  }
+
+}
+
+
 
 form.onsubmit = event => {
   event.preventDefault()
 
   const weight = inputWeight.value
   const height = inputHeight.value
-  
+
   const result = IMC(weight, height)
   const message = `Seu IMC é de ${result}`
 
-  modalMessage.innerText = message
-  modalWrapper.classList.add('open')
+  Modal.message.innerText = message
+  Modal.open()
 }
 
-modalBtnClose.onclick = () => {
-  modalWrapper.classList.remove('open')
+Modal.btnClose.onclick = () => {
+  Modal.close()
 }
 
 function IMC(weight, height) {
-  return (weight / ((height /100)**2)).toFixed(2)
+  return (weight / ((height / 100) ** 2)).toFixed(2)
 }
