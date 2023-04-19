48d3d0a9df236ac1bd1d8a8f28845886e73d592e
Marcio Tanan <marcio-tanan@hotmail.com>
2023-04-19 15:36:54 -0300

    Update script.js

--- Stage05/Focus Timer/script.js
+++ Stage05/Focus Timer/script.js
@@ -8,21 +8,21 @@ const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
 let minutes
 
-function countdown(){
-  setTimeout(function(){
-    let seconds =  Number(secondsDisplay.textContent)
+function countdown() {
+  setTimeout(function () {
+    let seconds = Number(secondsDisplay.textContent)
 
-    if(seconds <=0){
+    if (seconds <= 0) {
       seconds = 60
     }
 
     secondsDisplay.textContent = seconds - 1
     countdown()
-  },1000)
-  
+  }, 1000)
+
 }
 
-buttonPlay.addEventListener('click', function() {
+buttonPlay.addEventListener('click', function () {
   buttonPlay.classList.add('hide')
   buttonPause.classList.remove('hide')
   buttonSet.classList.add('hide')
@@ -31,31 +31,31 @@ buttonPlay.addEventListener('click', function() {
   countdown()
 })
 
-buttonPause.addEventListener('click', function() {
+buttonPause.addEventListener('click', function () {
   buttonPause.classList.add('hide')
   buttonPlay.classList.remove('hide')
   buttonSet.classList.remove('hide')
   buttonStop.classList.add('hide')
 })
 
-buttonStop.addEventListener('click', function() {
+buttonStop.addEventListener('click', function () {
   buttonPlay.classList.remove('hide')
   buttonPause.classList.add('hide')
   buttonSet.classList.remove('hide')
   buttonStop.classList.add('hide')
 })
 
-buttonSounOn.addEventListener('click', function() {
+buttonSounOn.addEventListener('click', function () {
   buttonSounOn.classList.add('hide')
   buttonSounOff.classList.remove('hide')
 })
 
-buttonSounOff.addEventListener('click', function() {
+buttonSounOff.addEventListener('click', function () {
   buttonSounOff.classList.add('hide')
   buttonSounOn.classList.remove('hide')
 })
 
-buttonSet.addEventListener('click', function() {
+buttonSet.addEventListener('click', function () {
   minutes = prompt('Quantos minutos')
   minutesDisplay.textContent = minutes
 })
