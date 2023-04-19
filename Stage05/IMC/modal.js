e99fea4df015b5e2f7c49020c765ce9dde78179c
Marcio Tanan <marcio-tanan@hotmail.com>
2023-04-19 08:44:30 -0300

    update project

---
+++ Stage05/IMC/modal.js
@@ -0,0 +1,19 @@
+const Modal = {
+
+  wrapper: document.querySelector('.modal-wrapper'),
+  message: document.querySelector('.modal .title span'),
+  btnClose: document.querySelector('.modal button.close'),
+
+
+  open: function() {
+    Modal.wrapper.classList.add('open')
+  },
+  close: function() {
+    Modal.wrapper.classList.remove('open')
+  }
+
+}
+
+Modal.btnClose.onclick = () => {
+  Modal.close()
+}
--- Stage05/IMC/script.js
+++ Stage05/IMC/script.js
@@ -2,24 +2,6 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-const Modal = {
-
-  wrapper: document.querySelector('.modal-wrapper'),
-  message: document.querySelector('.modal .title span'),
-  btnClose: document.querySelector('.modal button.close'),
-
-
-  open: function() {
-    Modal.wrapper.classList.add('open')
-  },
-  close: function() {
-    Modal.wrapper.classList.remove('open')
-  }
-
-}
-
-
-
 form.onsubmit = event => {
   event.preventDefault()
 
@@ -33,9 +15,7 @@ form.onsubmit = event => {
   Modal.open()
 }
 
-Modal.btnClose.onclick = () => {
-  Modal.close()
-}
+
 
 function IMC(weight, height) {
   return (weight / ((height / 100) ** 2)).toFixed(2)
