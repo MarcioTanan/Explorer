145c41477e5485abd792ff336c700702ae620655
Marcio Tanan <marcio-tanan@hotmail.com>
2023-04-23 08:56:07 -0300

    OOP

--- Stage06/SPA/js/index.js
+++ Stage06/SPA/js/index.js
@@ -1,5 +1,5 @@
 
-
+import './router.js'
 const routes = {
   "/": "Stage06/SPA/pages/home.html",
   "/about": "Stage06/SPA/pages/about.html",
@@ -7,27 +7,9 @@ const routes = {
   404: "Stage06/SPA/pages/404.html",
 }
 
-function route(event){
-  event = event || window.event
-  event.preventDefault()
 
-  window.history.pushState({},"", event.target.href)
 
-  handle()
-}
 
-function handle(){
-  const { pathname } = window.location
-  const route = routes[pathname] || routes[404]
-
-  fetch(route)
-  .then(data => data.text())
-  .then(html => {
-    
-    document.querySelector('#app').innerHTML = html
-  })
-
-}
 
   handle()
   window.onpopstate = () => handle()
