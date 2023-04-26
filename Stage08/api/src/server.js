1931d01d9cbbabaee4ce72f53a927e51b71337a5
Marcio Tanan <marcio-tanan@hotmail.com>
2023-04-26 09:30:35 -0300

    Update server.js

--- Stage08/api/src/server.js
+++ Stage08/api/src/server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 
-
 const app = express();
+
+
+
+app.get("/message/:id/:user", (request, response) => {
+  const { id, user} = request.params;
+  response.send(`Id da mensagem: ${id}. Para o usuário: ${user}.`);
+});
 
 const PORT = 3333;
 
