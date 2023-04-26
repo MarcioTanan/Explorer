b851dbf828b04737124e2074ec4266384c93f093
Marcio Tanan <marcio-tanan@hotmail.com>
2023-04-26 13:59:09 -0300

    add controllers

--- Stage08/api/src/routes/index.js
+++ Stage08/api/src/routes/index.js
@@ -1,9 +1,8 @@
 const {Router} = require("express");
 
-const usersRouter = require("./users.routes")
+const usersRouter = require("./users.routes");
 
-const routes = Router()
+const routes = Router();
+routes.use("/users", usersRouter);
 
-routes.use("./users", usersRouter)
-
-module.exports = routes
+module.exports = routes;
