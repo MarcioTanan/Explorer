70170357637cf03f37123e3acfc92e04bbd1f389
Marcio Tanan <marcio-tanan@hotmail.com>
2023-04-26 09:49:46 -0300

    Update server.js

--- Stage08/api/src/server.js
+++ Stage08/api/src/server.js
@@ -9,6 +9,11 @@ app.get("/message/:id/:user", (request, response) => {
   response.send(`Id da mensagem: ${id}. Para o usuário: ${user}.`);
 });
 
+app.get("/users", (request, response) => {
+  const { page , limit} = request.query;
+  response.send(`Página: ${page}. Mostrar: ${limit}`);
+});
+
 const PORT = 3333;
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
