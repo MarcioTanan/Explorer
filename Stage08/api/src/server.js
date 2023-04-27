ebe1b55e50df8cc205195730b5ed9ffe1d2cce8a
Marcio Tanan <marcio-tanan@hotmail.com>
2023-04-27 10:28:21 -0300

    update database

--- Stage08/api/src/server.js
+++ Stage08/api/src/server.js
@@ -1,6 +1,6 @@
 require("express-async-errors");
 
-const database = require("")
+const database = require("./database/sqlite")
 
 const AppError = require("./utils/AppError")
 
@@ -12,6 +12,8 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+database();
 
 app.use((error, request, response, next) => {
   if(error instanceof AppError) {
