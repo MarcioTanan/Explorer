7d1d4b7587064b85d12fee389f7f9dd8ee33df4e
Marcio Tanan <marcio-tanan@hotmail.com>
2023-04-28 10:45:53 -0300

    add update database

--- Stage08/api/src/controllers/UsersController.js
+++ Stage08/api/src/controllers/UsersController.js
@@ -13,16 +13,42 @@ class UsersController {
 
 
     if(checkUserExist){
-      throw new AppError('Este e-mail já está em uso')
+      throw new AppError('Este e-mail já está em uso.')
     }
+
+    const hashedPassword =await hash(password, 8);
 
     await database.run(
         'INSERT INTO users (name, email, password) VALUES (?,?,?)',
-        [name, email, password]
+        [name, email, hashedPassword]
        );
 
     return response.status(201).json();
 
+  }
+
+  async update(request, response) {
+    const { name , email } = request.body;
+    const {id } = request.params;
+
+    const database = await sqliteConnection();
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id]);
+
+    if(!user){
+      throw new AppError('Usuário não encontrado')
+    }
+
+    const userWithUpdatedEmail = await database.get('SELECT * FROM users WHERE email = (?)', [email])
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== id) {
+        throw new AppError('Este e-mail já está em uso.')
+    }
+
+    user.name = name;
+    user.email = email;
+
+    await database.run('UPDATE users SET name = ?, email = ?, update_at = ? WHERE id = ?, [user.name, user.email, new Date(), id]')
+    return response.status(200).json();
   }
 }
 module.exports = UsersController
