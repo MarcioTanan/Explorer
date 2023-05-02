242b71b8cce596437500accdb54a3a187a3945d1
Marcio Tanan <marcio-tanan@hotmail.com>
2023-05-02 10:14:46 -0300

    style css

--- Stage09/rocketnotes/src/main.jsx
+++ Stage09/rocketnotes/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Details } from './pages/Details/index.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./styles/global";
+import theme from "./styles/theme";
+import { Details } from "./pages/Details/index.jsx";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Details />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Details />
+    </ThemeProvider>
+  </React.StrictMode>
+);
