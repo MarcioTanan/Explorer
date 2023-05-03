da85ae3507180eaef3e6943ec51d9936dafc8f39
Marcio Tanan <marcio-tanan@hotmail.com>
2023-05-03 10:25:35 -0300

    create grid-area

--- Stage09/rocketnotes/src/main.jsx
+++ Stage09/rocketnotes/src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/global";
 import theme from "./styles/theme";
-import { Details } from "./pages/Details/index.jsx";
+import { Home } from "./pages/Home";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>
 );
--- Stage09/rocketnotes/src/pages/Home/styles.js
+++ Stage09/rocketnotes/src/pages/Home/styles.js
@@ -1,19 +1,44 @@
 import styled from 'styled-components';
 
-export const Container  = styled.div`
+export const Container = styled.div`
   width: 100%;
   height: 100vh;
 
   display: grid;
   grid-template-columns: 250px auto;
+  grid-template-rows: 105px 128px auto 64px;
+  grid-template-areas: 
+  "brand header"
+  "menu search"
+  "menu content"
+  "newnote content";
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+  
 `;
 
-export const Brand  = styled.div``;
+export const Brand = styled.div`
+  grid-area: brand;
+  background: red;
+`;
 
-export const Menu  = styled.ul``;
 
-export const Search  = styled.div``;
+export const Menu = styled.ul`
+  grid-area: menu;
+  background: green;
+`;
 
-export const Content  = styled.div``;
+export const Search = styled.div`
+  grid-area: search;
+  background: violet;
+`;
 
-export const NewNote = styled.buttom``;
+export const Content = styled.div`
+  grid-area: content;
+  background: blue;
+`;
+
+export const NewNote = styled.button`
+  grid-area: newnote;
+  background: yellow;
+`;
