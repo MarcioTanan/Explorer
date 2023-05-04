4f61b1785e2acb27d141a02d24bbe08f9da3b29a
Marcio Tanan <marcio-tanan@hotmail.com>
2023-05-04 09:03:09 -0300

    Create_notes

--- Stage09/rocketnotes/src/pages/Home/index.jsx
+++ Stage09/rocketnotes/src/pages/Home/index.jsx
@@ -1,8 +1,10 @@
-import {FiPlus, FiSearch} from 'react-icons/fi';
+import { FiPlus, FiSearch } from "react-icons/fi";
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 
-import { Header } from "../../components/Header";
+import { Note } from "../../components/Note";
 import { Input } from "../../components/Input";
+import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
 import { ButtonText } from "../../components/ButtonText";
 
 export function Home() {
@@ -27,13 +29,24 @@ export function Home() {
       </Menu>
 
       <Search>
-        <Input placeholder='Pesquisar pelo título' icon={FiSearch}/>
+        <Input placeholder="Pesquisar pelo título" icon={FiSearch} />
       </Search>
 
-      <Content></Content>
+      <Content>
+        <Section title="Minhas notas">
+          <Note data={{
+            title: 'React Modal',
+             tags:[
+              {id: '1',name: 'react'},
+              {id: '2',name: 'rocketseat'}
+            ]
+             }}
+              />
+        </Section>
+      </Content>
 
       <NewNote>
-        <FiPlus/>
+        <FiPlus />
         Criar Nota
       </NewNote>
     </Container>
