a899f25d50904d8a01d96c727d0a347fe2be1ef5
Marcio Tanan <marcio-tanan@hotmail.com>
2023-05-04 11:14:37 -0300

    Create_SignUp

--- Stage09/rocketnotes/src/main.jsx
+++ Stage09/rocketnotes/src/main.jsx
@@ -6,13 +6,14 @@ import GlobalStyle from "./styles/global";
 import theme from "./styles/theme";
 
 
-import { SingIn } from "./pages/SignIn";
+
+import { SingUp } from "./pages/SignUp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <SingIn />
+      <SingUp />
     </ThemeProvider>
   </React.StrictMode>
 );
--- Stage09/rocketnotes/src/pages/SignUp/index.jsx
+++ Stage09/rocketnotes/src/pages/SignUp/index.jsx
@@ -1,31 +1,34 @@
-import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
+import { FiLogIn, FiMail, FiLock, FiUsers } from "react-icons/fi";
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
 import { Container, Form, Background } from "./styles";
 
-export function SingIn() {
+export function SingUp() {
   return (
     <Container>
+    <Background/>
       <Form>
         <h1>Rocket Notes</h1>
-        <p>Aplicação para salvar e gerenciar seu links úteis.</p>
-        <h2>Faça seu login</h2>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+        <h2>Crie sua conta</h2>
 
+        <Input placeholder="Nome" type="text" icon={FiUsers} />
+        
         <Input placeholder="E-mail" type="text" icon={FiMail} />
 
         <Input placeholder="Senha" type="password" icon={FiLock} />
 
-        <Button title='Entrar'/>
+        <Button title='Cadastrar'/>
 
        <a href="#">
-        Criar conta
+        Voltar para o login
        </a>
         
       </Form>
 
-      <Background/>
+  
     </Container>
   );
 }
