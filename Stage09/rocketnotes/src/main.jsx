0b8af1f3e518d1992bb5eab2c3640568e4493175
Marcio Tanan <marcio-tanan@hotmail.com>
2023-05-04 10:46:32 -0300

    Create_form

--- Stage09/rocketnotes/src/main.jsx
+++ Stage09/rocketnotes/src/main.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/global";
+
 import theme from "./styles/theme";
-import { Home } from "./pages/Home";
+
+
+import { SingIn } from "./pages/SignIn";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Home />
+      <SingIn />
     </ThemeProvider>
   </React.StrictMode>
 );
