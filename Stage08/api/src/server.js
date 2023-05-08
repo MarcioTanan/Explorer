1064336d46dd851e35a4a2eb7c6af78b48edd734
Marcio Tanan <marcio-tanan@hotmail.com>
2023-05-08 15:40:36 -0300

    Create_API

--- Stage08/api/src/server.js
+++ Stage08/api/src/server.js
@@ -1,19 +1,23 @@
 require("express-async-errors");
 const AppError = require("./utils/AppError")
+const uploadConfig = require("./configs/upload")
 
-
+const cors = require("cors")
 const migrationRun = require("./database/sqlite/migration")
 const express = require("express");
 
 const routes = require("./routes/index.js")
 
+migrationRun();
+
 
 const app = express();
 
 app.use(express.json());
-app.use(routes);
+app.use(cors())
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 
-migrationRun();
+app.use(routes);
 
 app.use((error, request, response, next) => {
   if(error instanceof AppError) {
