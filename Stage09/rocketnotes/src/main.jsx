5f1426325ac93511b1cee6015e7e350f5b423ad4
Marcio Tanan <marcio-tanan@hotmail.com>
2023-05-09 10:11:58 -0300

    Create_Context

--- Stage09/rocketnotes/src/main.jsx
+++ Stage09/rocketnotes/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/global";
 
+import { MyContext } from "./myContext";
+
 import { Routes } from "./routes";
 
 import theme from "./styles/theme";
@@ -13,7 +15,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <MyContext.Provider value={{email: 'marcio@email.com'}}>
       <Routes />
+      </MyContext.Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
