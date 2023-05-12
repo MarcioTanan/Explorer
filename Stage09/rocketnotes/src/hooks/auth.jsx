a008c0845259936b42da8dcd1530110a4004914f
Marcio Tanan <marcio-tanan@hotmail.com>
2023-05-12 16:00:27 -0300

    local_storage

--- Stage09/rocketnotes/src/hooks/auth.jsx
+++ Stage09/rocketnotes/src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 import { api } from "../services/api";
 
@@ -14,6 +14,10 @@ function AuthProvider({ children }) {
 
       api.defaults.headers.authorization = `Bearer ${token}`;
       setData({ user, token });
+
+      localStorage.setItem("@rocketnotes:user",JSON.stringify(user))
+      localStorage.setItem("@rocketnotes:token", token );
+    
     } catch (error) {
       if (error.response) {
         alert(error.response.data.message);
@@ -23,6 +27,23 @@ function AuthProvider({ children }) {
     }
     
   }
+
+  useEffect(() =>{
+    const token = localStorage.getItem("@rocketnotes:token")
+    const user = localStorage.getItem("@rocketnotes:user")
+
+    if(token && user){
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
+      setData({
+        token,
+        user: JSON.parse(user)
+      })
+    }
+  }, [])
+
+
+
   return (
     <AuthContext.Provider value={{ signIn, user: data.user }}>
       {children}
