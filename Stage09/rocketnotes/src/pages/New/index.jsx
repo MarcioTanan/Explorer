b9a0d6b24fb919f0ab07ee46991919285f3903f0
Marcio Tanan <marcio-tanan@hotmail.com>
2023-05-15 14:24:30 -0300

    Update index.jsx

--- Stage09/rocketnotes/src/pages/New/index.jsx
+++ Stage09/rocketnotes/src/pages/New/index.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { Container, Form } from "./styles";
+
+
 import { Textarea } from "../../components/Textarea";
 import { NoteItem } from "../../components/NoteItem";
 import { Section } from "../../components/Section";
@@ -6,9 +10,16 @@ import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
 import {Link} from 'react-router-dom'
 
-import { Container, Form } from "./styles";
 
 export function New() {
+  const [links, setLinks] = useState([])
+  const [newLink, setNewLink] = useState("")
+
+  function handleAddLink(){
+    setLinks(prevState => [...prevState, newLink] )
+    setNewLink("")
+  }
+
   return (
     <Container>
       <Header />
@@ -25,8 +36,14 @@ export function New() {
           <Textarea placeholder="Observações" />
 
           <Section title="Links úteis">
-            <NoteItem value="https://rocketseat.com.br" />
-            <NoteItem isNew placeholder="Novo link" />
+
+            {
+              links.map((link, index) =>(
+                <NoteItem key={String(index)} value={link}  onClick={() =>{}}/>
+              ))
+            }
+          
+            <NoteItem isNew placeholder="Novo link" value={newLink} onChange={e => setNewLink(e.target.value)} onClick={handleAddLink}/>
           </Section>
 
           <Section title="Marcadores">
