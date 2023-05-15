7c936b711f76ce56f534d7be4de20755120f22e5
Marcio Tanan <marcio-tanan@hotmail.com>
2023-05-15 15:28:32 -0300

    Update auth.jsx

--- Stage09/rocketnotes/src/hooks/auth.jsx
+++ Stage09/rocketnotes/src/hooks/auth.jsx
@@ -12,12 +12,11 @@ function AuthProvider({ children }) {
       const response = await api.post("/sessions", { email, password });
       const { user, token } = response.data;
 
-      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
       setData({ user, token });
 
-      localStorage.setItem("@rocketnotes:user",JSON.stringify(user))
-      localStorage.setItem("@rocketnotes:token", token );
-    
+      localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
+      localStorage.setItem("@rocketnotes:token", token);
     } catch (error) {
       if (error.response) {
         alert(error.response.data.message);
@@ -25,73 +24,64 @@ function AuthProvider({ children }) {
         alert("Não foi possível entrar.");
       }
     }
-    
   }
 
   function signOut() {
-   localStorage.removeItem("@rocketnotes:token")
-   localStorage.removeItem("@rocketnotes:user")
+    localStorage.removeItem("@rocketnotes:token");
+    localStorage.removeItem("@rocketnotes:user");
 
-   setData({})
+    setData({});
   }
 
-  async function updateProfile({user, avatarFile}) {
-    try{
+  async function updateProfile({ user, avatarFile }) {
+    try {
+      if (avatarFile) {
+        const fileUploadForm = new FormData();
+        fileUploadForm.append("avatar", avatarFile);
 
-      if(avatarFile){
-        const fileUploadForm = new FormData()
-        fileUploadForm.append("avatar", avatarFile)
+        const response = await api.patch("/users/avatar", fileUploadForm);
 
-        const response = await api.patch("/users/avatar", fileUploadForm)
-
-        user.avatar = response.data.avatar
+        user.avatar = response.data.avatar;
       }
 
-      await api.put("/users",  user );
-      localStorage.setItem("@rocketnotes:user", JSON.stringify(user))
+      await api.put("/users", user);
+      localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
 
       setData({ user, token: data.token });
-      alert("Perfil atualizado!")
-
-    }catch (error) {
+      alert("Perfil atualizado!");
+    } catch (error) {
       if (error.response) {
         alert(error.response.data.message);
       } else {
         alert("Não foi possível atualizar o perfil.");
       }
+    }
   }
-}
 
+  useEffect(() => {
+    const token = localStorage.getItem("@rocketnotes:token");
+    const user = localStorage.getItem("@rocketnotes:user");
 
-
-
-    
-  useEffect(() =>{
-    const token = localStorage.getItem("@rocketnotes:token")
-    const user = localStorage.getItem("@rocketnotes:user")
-
-    if(token && user){
-         api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-
+    if (token && user) {
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
       setData({
         token,
-        user: JSON.parse(user)
-      })
+        user: JSON.parse(user),
+      });
     }
-  }, [])
-
-
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ 
-      signIn,
-      signOut,
-      updateProfile,
-       user: data.user, 
-       }}
-       >
-        {children}
+    <AuthContext.Provider
+      value={{
+        signIn,
+        signOut,
+        updateProfile,
+        user: data.user,
+      }}
+    >
+      {children}
     </AuthContext.Provider>
   );
 }
