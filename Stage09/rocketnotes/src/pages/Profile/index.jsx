39faa5d2d5bedb2c0c2800ed79166c3a01c4a7a5
Marcio Tanan <marcio-tanan@hotmail.com>
2023-05-19 10:02:36 -0300

    update_project

--- Stage09/rocketnotes/src/pages/Profile/index.jsx
+++ Stage09/rocketnotes/src/pages/Profile/index.jsx
@@ -31,14 +31,15 @@ export function Profile() {
 
 
   async function handleUpdate(){
-      const user ={
+      const updated ={
         name,
         email,
         password: passwordNew,
-        old_password: passwordOld,
+        old_password: passwordOld
       }
+      const userUpdated = Object.assign(user, updated)
     
-    await updateProfile({user, avatarFile})
+    await updateProfile({user:userUpdated, avatarFile})
   }
 
 
