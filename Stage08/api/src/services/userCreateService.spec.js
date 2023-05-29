186e30fd56e5d68a88bde3b5e68d79cc4bc441e2
Marcio Tanan <marcio-tanan@hotmail.com>
2023-05-29 08:37:05 -0300

    Update userCreateService.spec.js

--- Stage08/api/src/services/userCreateService.spec.js
+++ Stage08/api/src/services/userCreateService.spec.js
@@ -1,7 +1,16 @@
 const UserCreateService = require("./UserCreateService")
 const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory")
+const AppError = require('../utils/AppError');
 
 describe("UserCreateService", () => {
+
+   let userRepositoryInMemory = null
+   let userCreateService = null
+
+   beforeEach(() => {
+      userRepositoryInMemory = new UserRepositoryInMemory
+      userCreateService = new UserCreateService(userRepositoryInMemory);
+   })
 
    it("user should be create", async ()=>{
       const user = {
@@ -10,8 +19,8 @@ describe("UserCreateService", () => {
        password:"123"
       }
    
-   const userRepositoryInMemory = new UserRepositoryInMemory
-   const userCreateService = new UserCreateService(userRepositoryInMemory);
+
+  
    const userCreated = await userCreateService.execute(user);
    
    expect(userCreated).toHaveProperty("id")
@@ -31,10 +40,8 @@ describe("UserCreateService", () => {
          password: "456"
       }
 
-      const userRepository = new UserRepositoryInMemory()
-      const userCreateService = new UserCreateService(userRepository)
-
       await userCreateService.execute(user1)
+      await expect(userCreateService.execute(user2)).rejects.toEqual(new AppError("Este e-mail já está em uso."))
    })
 
 
